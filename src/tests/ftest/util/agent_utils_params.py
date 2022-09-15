1efe47c27fd2d7b6b91fcfe097d9640dc23277c0
Dalton Bohning <dalton.bohning@intel.com>
2022-09-15 23:16:11 +0000

    DAOS-11621 test: support specifying fabric_ifaces
    WIP
    
    Signed-off-by: Dalton Bohning <dalton.bohning@intel.com>

--- src/tests/ftest/util/agent_utils_params.py
+++ src/tests/ftest/util/agent_utils_params.py
@@ -30,6 +30,53 @@ class DaosAgentTransportCredentials(TransportCredentials):
 class DaosAgentYamlParameters(YamlParameters):
     """Defines the daos_agent configuration yaml parameters."""
 
+    # TODO model after server_utils_params
+    class FabricIfaceDeviceYamlParameters(YamlParameters):
+        """Defines the fabric_iface device yaml parameters."""
+
+        def __init__(self, iface_index=None, device_index=None):
+            """Initialize a FabricIfaceDeviceYamlParameters object.
+
+            Args:
+                iface_index (int, optional): index number for the namespace path used
+                    when specifying multiple fabric_ifaces. Defaults to None.
+                device_index (int, optional): index number for the namespace path used
+                    when specifying multiple devices. Defaults to None.
+            """
+            iface_str = "/{}".format(iface_index) if iface_index is not None else ""
+            dev_str = "/{}".format(device_index) if device_index is not None else ""
+            namespace = "/run/agent_config/fabric_ifaces{}/devices{}/*".format(iface_str, dev_str)
+            super().__init__(namespace)
+
+            # Parameters
+            #   iface:              TODO
+            #   domain:             TODO
+            self.iface = BasicParameter(None)
+            self.domain = BasicParameter(None)
+
+    class FabricIfaceYamlParameters(YamlParameters):
+        """Defines the fabric_iface yaml parameters."""
+
+        def __init__(self, iface_index=None, device_index=None):
+            """Initialize a FabricIfaceYamlParameters object.
+
+            Args:
+                iface_index (int, optional): index number for the namespace path used
+                    when specifying multiple fabric_ifaces. Defaults to None.
+                device_index (int, optional): index number for the namespace path used
+                    when specifying multiple devices. Defaults to None.
+            """
+            iface_str = "/{}".format(iface_index) if iface_index is not None else ""
+            dev_str = "/{}".format(device_index) if device_index is not None else ""
+            namespace = "/run/agent_config/fabric_ifaces{}/devices{}/*".format(iface_str, dev_str)
+            super().__init__(namespace)
+
+            # Parameters
+            #   iface:              TODO
+            #   domain:             TODO
+            self.iface = BasicParameter(None)
+            self.domain = BasicParameter(None)
+
     def __init__(self, filename, common_yaml):
         """Initialize an DaosAgentYamlParameters object.
 
@@ -53,6 +100,7 @@ class DaosAgentYamlParameters(YamlParameters):
         self.runtime_dir = BasicParameter(None, "/var/run/daos_agent")
         self.log_file = LogParameter(log_dir, None, "daos_agent.log")
         self.control_log_mask = BasicParameter(None, "debug")
+        self.fabric_ifaces = []
 
     def update_log_file(self, name):
         """Update the log file name for the daos agent.
@@ -65,3 +113,59 @@ class DaosAgentYamlParameters(YamlParameters):
         """
         if name is not None:
             self.log_file.update(name, "agent_config.log_file")
+
+class PerEngineYamlParameters(YamlParameters):
+        """Defines the configuration yaml parameters for a single server."""
+
+        # Engine environment variables that are required by provider type.
+        REQUIRED_ENV_VARS = {
+            "common": [
+                "D_LOG_FILE_APPEND_PID=1",
+                "COVFILE=/tmp/test.cov"],
+            "ofi+tcp": [
+                "SWIM_PING_TIMEOUT=10"],
+            "ofi+verbs": [
+                "FI_OFI_RXM_USE_SRX=1"],
+            "ofi+cxi": [
+                "FI_OFI_RXM_USE_SRX=1",
+                "CRT_MRC_ENABLE=1"],
+        }
+
+        def __init__(self, index=None, provider=None):
+            """Create a SingleServerConfig object.
+
+            Args:
+                index (int, optional): index number for the namespace path used
+                    when specifying multiple servers per host. Defaults to None.
+            """
+            namespace = "/run/server_config/servers/*"
+            if isinstance(index, int):
+                namespace = "/run/server_config/servers/{}/*".format(index)
+            super().__init__(namespace)
+            if provider is not None:
+                self._provider = provider
+            else:
+                self._provider = os.environ.get("CRT_PHY_ADDR_STR", "ofi+tcp")
+
+            # Use environment variables to get default parameters
+            default_interface = os.environ.get("DAOS_TEST_FABRIC_IFACE", "eth0")
+            default_port = int(os.environ.get("OFI_PORT", 31416))
+            default_share_addr = int(os.environ.get("CRT_CTX_SHARE_ADDR", 0))
+
+            # All log files should be placed in the same directory on each host
+            # to enable easy log file archiving by launch.py
+            log_dir = os.environ.get("DAOS_TEST_LOG_DIR", "/tmp")
+
+            # Parameters
+            #   targets:                I/O service threads per engine
+            #   first_core:             starting index for targets
+            #   nr_xs_helpers:          I/O offload threads per engine
+            #   fabric_iface:           map to OFI_INTERFACE=eth0
+            #   fabric_iface_port:      map to OFI_PORT=31416
+            #   log_mask:               map to D_LOG_MASK env
+            #   log_file:               map to D_LOG_FILE env
+            #   env_vars:               influences DAOS I/O Engine behavior
+            #       Add to enable scalable endpoint:
+            #           - CRT_CTX_SHARE_ADDR=1
+            #           - CRT_CTX_NUM=8
+            self.targets = BasicParameter(None, 8)
